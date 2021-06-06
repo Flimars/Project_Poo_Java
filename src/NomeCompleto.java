3d3285b1ffd7043066934152dbb9c39a011ab4b3
Flavio Lima <flavio.lima@aluno.riogrande.ifrs.edu.br>
2021-06-06 01:47:57 -0300

    Implementar método Nome Completo.

---
+++ src/NomeCompleto.java
@@ -0,0 +1,12 @@
+public class NomeCompleto {
+    public static String todonome (String nome, String sobrenome) {
+
+        String nome = "";
+        String sobrenome = "";
+        String resultado = nome + sobrenome;
+
+        return resultado; 
+        
+    }
+    
+}
