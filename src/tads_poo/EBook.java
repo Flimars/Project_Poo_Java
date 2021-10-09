3969082845bfad7491a2fc2c54ad092ff61c5c48
Flimars <flaviolima.pop@gmail.com>
2021-10-09 15:00:57 -0300

    EBook: Desenvolvendo classe EBook.

---
+++ src/tads_poo/EBook.java
@@ -0,0 +1,13 @@
+package src.tads_poo;
+
+public class EBook {
+    final String titulo;
+    final int paginas;
+    int numeroPagina;
+
+    EBook(String titulo, int paginas) {
+        this.titulo = titulo;
+        this.paginas = paginas;
+    }
+    
+}
